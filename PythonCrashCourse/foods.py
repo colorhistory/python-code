f05c403a499805560f5af50f4b6e24707bf9dca2
whitehistory <xiaohaidotpro@outlook.com>
2019-07-08 22:07:03 -0400

    new

---
+++ PythonCrashCourse/foods.py
@@ -0,0 +1,8 @@
+myFoods = ["pizza", "falafel", "carrot cake"]
+friendFoods = myFoods[:]
+
+print("My favorite foods are:")
+print(myFoods)
+
+print("\nMy friend's favorite foods are:")
+print(friendFoods)
